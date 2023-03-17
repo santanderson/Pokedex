3dedaabffcf20de26c6ac5d35b979109acddf496
Ryan <santoryan1408@gmail.com>
2023-03-17 18:19:42 -0300

    Adicionou estrutura dos detalhes dos pokemon

---
+++ src/views/Pokemon.vue
@@ -0,0 +1,39 @@
+<script setup>
+
+const pokemon = {
+    name: "Zekrom",
+    id: "644",
+    common: "Deep Black Pokémon",
+    description: "lorem",
+    types: ["dragon", "electric"]
+}
+</script>
+
+<template>
+    <main>
+        <div>
+            <div class="pkmView">
+                <img/>
+            </div>
+            <div class="details">
+                <div>
+                    {{ pokemon.id }}
+                    {{ pokemon.name }}
+                </div>
+                {{ pokemon.common }}
+                <div class="types">
+                    {{ pokemon.types[0] }}
+                    {{ pokemon.types[1] }}
+                </div>
+            </div>
+        </div>
+
+        <div class="description">
+            <p>{{ pokemon.description }}</p>
+        </div>
+    </main>
+
+    <footer>
+        <button>Back</button>
+    </footer>
+</template>
